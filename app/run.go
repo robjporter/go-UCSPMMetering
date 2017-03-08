e1cce2d6165a21b2ed9ffa30d5c1b40b18401174
robjporter <robporter79@outlook.com>
2017-03-08 11:52:30 +0000

    Removed a structural folder
    - Removed a structural folder that was used to help refine the application structure moving forward
    - Update run stage 2, to terminate after EULA acceptance and forcing a restart

--- app/run.go
+++ app/run.go
@@ -9,8 +9,8 @@ import (
 func (a *Application) RunStage2() {
 	a.LogInfo("Entering Run stage 2", nil, false)
 	if a.Config.GetBool("eula.agreed") {
-
 		a.LogInfo("EULA has been agreed to", nil, false)
+		a.RunStage3()
 	} else {
 		a.LogInfo("EULA has not yest been accepted.", nil, false)
 		answer := eula.DisplayEULA()
@@ -18,12 +18,13 @@ func (a *Application) RunStage2() {
 			a.Config.Set("eula.agreed", true)
 			a.saveConfig()
 			a.LogInfo("EULA acceptance state has been updated....Thankyou.", nil, false)
+			a.LogInfo("Please rerun the application to continue.", nil, false)
+			os.Exit(0)
 		} else {
 			a.LogInfo("The application cannot continue unless the EULA is accepted.", nil, false)
 			os.Exit(0)
 		}
 	}
-	a.RunStage3()
 }
 
 func (a *Application) RunStage3() {
